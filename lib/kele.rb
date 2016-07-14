06f2e7ba28f68bf26b66094420000ad3ef6e2efe
Daniel Lisa <danlisa89@gmail.com>
2016-07-14 02:26:27 -0400

    added users

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,9 +3,22 @@ require 'httparty'
 class Kele
   include HTTParty
 
-  def initialize(username, password)
-    @username = username
-    @password = password
+  def initialize(email, password)
+    @base_uri = 'https://www.bloc.io/api/v1'
+    @options = { query: { email: email, password: password } }
   end
 
+  def auth_token
+    hash = Kele.post("#{@base_uri}/sessions", @options)
+    if hash["auth_token"]
+      hash["auth_token"]
+    else
+      hash
+    end
+  end
+
+  def get_me
+    response = Kele.get("#{@base_uri}/users/me", headers: { "authorization" => auth_token })
+    JSON.parse response.body
+  end
 end
